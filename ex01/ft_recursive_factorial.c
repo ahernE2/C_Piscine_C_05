2ec19755e11d3c8d0e5d6631eac43320a5c7884a
Alejandro Hernandez Hernandez <alejhern@cbr4s3.42barcelona.com>
2024-04-08 21:10:35 +0200

    Ejercicio 1

---
+++ ex01/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 20:48:53 by alejhern          #+#    #+#             */
+/*   Updated: 2024/04/08 21:09:21 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb == 1 || nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb -1));
+}
