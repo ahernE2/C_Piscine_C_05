e0141889b8a1b0aa58b94d89577b60e2781c0b63
Alejandro Hernandez Hernandez <alejhern@cbr4s3.42barcelona.com>
2024-04-08 21:09:52 +0200

    Ejercicio 1

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 20:40:35 by alejhern          #+#    #+#             */
+/*   Updated: 2024/04/08 20:59:10 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	while (nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
