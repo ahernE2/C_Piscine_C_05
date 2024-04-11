d906a7e615c7b6170dcff30b3c8f0c602d7c8d43
Alejandro Hernandez Hernandez <alejhern@cbr3s3.42barcelona.com>
2024-04-11 15:02:06 +0200

    ex07

---
+++ ex07/ft_find_next_prime.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 15:00:30 by alejhern          #+#    #+#             */
+/*   Updated: 2024/04/11 15:00:35 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	else if (nb == 2)
+		return (1);
+	while (nb >= i * i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
